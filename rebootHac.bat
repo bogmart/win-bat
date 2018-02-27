58834ebe958521e99390ea1b48fea1a20a62ec93
bogmart <36724163+bogmart@users.noreply.github.com>
2018-02-27 09:56:58 +0200

    rebootHAC: P4 and P5

--- rebootHac.bat
+++ rebootHac.bat
@@ -4,7 +4,10 @@
 SETLOCAL
 
 ::hmAction
-set oid_reboot=.1.3.6.1.4.1.248.14.2.1.0
+set oid_reboot_P4=.1.3.6.1.4.1.248.14.2.1.0
+
+::hm2DevMgmtActionReset (1= other, 2 = reset)
+set oid_reboot_P5=.1.3.6.1.4.1.248.11.10.1.2.1.0
 
 :HAC_IP
    set hac_ip=%1
@@ -13,7 +16,8 @@ IF .%hac_ip%.==.. GOTO HELP
 
 :REBOOT
    echo  rebooting...
-   snmpset -v 2c -c private  %hac_ip%  %oid_reboot% i 2    >nul 2>nul
+   snmpset -v 2c -c private -r 1 -t 1  %hac_ip%  %oid_reboot_P4% i 2    >nul 2>nul
+   snmpset -v 2c -c private -r 1 -t 1  %hac_ip%  %oid_reboot_P5% i 2    >nul 2>nul
    GOTO END
 
 :HELP
