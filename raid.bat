c97d9a8dad8b1557160ca81bafb07b666a9ca2a9
bogmart <36724163+bogmart@users.noreply.github.com>
2018-04-05 15:53:26 +0300

    raid bat updated to RSTe 4.6

--- raid.bat
+++ raid.bat
@@ -1,1 +1,2 @@
-C:\SWSetup\SP72212_RSTe\RSTe_4.3.0.1199_RSTe_CLI\rstcli64.exe --information
+::C:\SWSetup\SP72212_RSTe\RSTe_4.3.0.1199_RSTe_CLI\rstcli64.exe --information
+C:\SWSetup\RSTe_and_AHCI_Win_Drivers_GUI_CLI_CIM_4.6.0.1085\CLI\x64\rstcli64.exe --information
