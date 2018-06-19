c652bbc65510a0a97ec90b30ec66ec7ae52cae75
bogmart <36724163+bogmart@users.noreply.github.com>
2018-06-19 10:40:13 +0300

    binSize: add more options and objdump details

--- binSize.bat
+++ binSize.bat
@@ -1,31 +1,40 @@
 @ECHO OFF
 
-IF     .%1. == ..   GOTO HELP
+IF     .%1.    == ..   GOTO HELP
 
 set build=%1
+set device=%2
+IF .%device%.  == ..  set device=.*
+set release=%3
+IF .%release%. == ..  set release=v99999
+
+
 set tempFileBins=%tmp%\%build%_bins.txt
 set tempFileDbg=%tmp%\%build%_dbg.txt
 set tempBinCopyFile=%tmp%\tmp.bin
 
-rem dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\v07200\%build%\*.bin | grep -v "BOOT\|MRP\|PRP\|HSR\|DLR\|NAT" > %tempFileBins%
+
+
 
 if .%build%. == .all. (
-rem    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\v07200\*corefile | grep -v "boot"  | grep  "MACH" > %tempFileDbg%
+    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\*.bin | grep -i "%device%-" | grep -v "BOOT\|MRP\|PRP\|HSR\|DLR\|NAT" > %tempFileBins%
+    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\*corefile | grep -i "%device%" | grep -v "boot"  | grep  "%device%_" > %tempFileDbg%
 ) else (
-rem    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\v07200\%build%\*corefile  | grep -v "boot"  | grep  "MACH" > %tempFileDbg%
+    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\%build%\*.bin | grep -i "%device%-" | grep -v "BOOT\|MRP\|PRP\|HSR\|DLR\|NAT" > %tempFileBins%
+    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\%build%\*corefile  | grep -i "%device%" | grep -v "boot"  | grep  "%device%_" > %tempFileDbg%
 )	
 
 for /f  "eol=; delims=;" %%i in (%tempFileBins%) do (
     ::echo %%i
 	cp %%i %tempBinCopyFile%
-	call hmImgTool -l -i %tempBinCopyFile%  | grep -m 4 -i "Base\|Size" | tr "\n" "  "
+	call hmImgTool -l -i %tempBinCopyFile%  | grep -i "Name\|Size" | sed "s/.*: *$//" | sed "s/ *: */:/" | sed "s/ /_/" | tr "\n\r" " "
 	echo.
 )
 
-for /f  "eol=; delims=;" %%i in (%tempFileDbg%) do (
-    echo %%i
-	call objdump -h %%i | grep "vfp11_veneer"
+for /f  "eol=; delims=; " %%i in (%tempFileDbg%) do (
 	echo.
+    echo %%i
+	call objdump -h %%i | grep " [0-9]* \." | sed "s/ *[0-9]* *\(\.[^ ]* *[0-9a-f$]*\) .*/\1/"
 )
 
 
@@ -34,10 +43,10 @@ GOTO END
 
 :HELP
     echo.
-    ECHO usage:  %0  [build]
+    ECHO usage:  %0  [build]  [device]  [release]
     echo.
     ECHO    example:
-    ECHO       %0  NTLY122
+    ECHO       %0  NTLY122  ees  v99999
     @ECHO OFF
     GOTO END
 
