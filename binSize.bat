d32b831838394e11703a717d7d34a9a36ff0d4cc
bogmart <36724163+bogmart@users.noreply.github.com>
2018-07-25 11:19:01 +0300

    binSize - added OEM

--- binSize.bat
+++ binSize.bat
@@ -17,11 +17,11 @@ set tempBinCopyFile=%tmp%\tmp.bin
 
 
 if .%build%. == .all. (
-    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\*.bin | grep -i "%device%-" | grep -v "BOOT\|MRP\|PRP\|HSR\|DLR\|NAT" > %tempFileBins%
-    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\*corefile | grep -i "%device%" | grep -v "boot"  | grep  "%device%_" > %tempFileDbg%
+    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\*.bin | grep -i "%device%-" | grep -v "BOOT\|MRP\|PRP\|HSR\|DLR\|NAT\|_VL\|_ABB" > %tempFileBins%
+    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\*corefile | grep -i "%device%" | grep -v "boot\|_VL\|_ABB"  | grep  "%device%_" > %tempFileDbg%
 ) else (
-    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\%build%\*.bin | grep -i "%device%-" | grep -v "BOOT\|MRP\|PRP\|HSR\|DLR\|NAT" > %tempFileBins%
-    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\%build%\*corefile  | grep -i "%device%" | grep -v "boot"  | grep  "%device%_" > %tempFileDbg%
+    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\%build%\*.bin | grep -i "%device%-" | grep -v "BOOT\|MRP\|PRP\|HSR\|DLR\|NAT\|_VL\|_ABB" > %tempFileBins%
+    dir /b  /s \\10.115.43.16\K-Stufen\PlattformV\%release%\%build%\*corefile  | grep -i "%device%" | grep -v "boot\|_VL\|_ABB"  | grep  "%device%_" > %tempFileDbg%
 )	
 
 for /f  "eol=; delims=;" %%i in (%tempFileBins%) do (
@@ -34,8 +34,9 @@ for /f  "eol=; delims=;" %%i in (%tempFileBins%) do (
 for /f  "eol=; delims=; " %%i in (%tempFileDbg%) do (
 	echo.
     echo %%i
-	call objdump -h %%i | grep " [0-9]* \." | sed "s/ *[0-9]* *\(\.[^ ]* *[0-9a-f$]*\) .*/\1/"
+	call objdump -h %%i | grep " [0-9]* \." | sed "s/ *[0-9]* *\(\.[^ ]* *[0-9a-f$]*\) .*/\1/" 
 )
+::| tr [:lower:] [:upper:] | bc
 
 
 GOTO END
